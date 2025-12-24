6cec73cc42e0e03a5537a44c93347d6e3e2c2def
Nathan Sudiara <143156875+Vallykrie@users.noreply.github.com>
2025-12-24 12:08:12 +0700

    Add automation script for SIAM questionnaire
    This script automates the selection of radio buttons and submission of comments on the SIAM questionnaire.

---
+++ kuisioner-siam-ub.js
@@ -0,0 +1,27 @@
+// © 2023 Pande Kadek Nathan Prabhaswara
+// Cara pakai:
+// 1. Buka halaman SIAM yang berisi pertanyaan kuesioner
+// 2. Buka DevTools Chrome (F12 atau Ctrl + Shift + i)
+// 3. Ketik "allow paste" lalu Enter
+// 4. Copas kode ini di console
+// 5. Tekan Enter
+
+// Masukkan option yang akan dipilih
+// 1 = sangat tidak baik
+// 2 = tidak baik
+// 3 = biasa
+// 4 = baik
+// 5 = sangat baik
+const value = 4
+
+// Masukkan komentar
+const saran = "isi komentar kamu"
+
+var inputElements = document.getElementsByTagName("input");
+  for (var i=0; i<inputElements.length; i++) 
+    {
+      if (inputElements[i].getAttribute('type') == 'radio' && inputElements[i].getAttribute('value') == value) 
+        {inputElements[i].checked = true;
+        }
+      }
+jQuery("#saran").html(saran);
